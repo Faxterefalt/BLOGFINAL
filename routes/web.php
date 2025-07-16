f47b8ea5eb782568ca629c189d6412bc1c6f6932
Faxter <fabianazuga123hotmail.com>
2025-07-16 16:13:05 -0400

    rutas con nombre

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,12 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-//blog.test => welcome (devuelve la vista welcome.blade.php)
+
 Route::view('/','welcome');
 
-//blog.test/contacto = contact (devuelve la vista contact.blade.php)
-Route::view('/contacto','contact');
-//blog.test/blog => blog (devuelve la vista blog.blade.php)
-Route::view('/blog','blog');
-//blog.test/blog/nosotros => about (devuelve la vista about.blade.php)
-Route::view('/nosotros','about');
+Route::view('/contact','contact')->name('contact');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::view('/blog','blog')->name('blog');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::view('/nosotros','about')->name('about');
 
-require __DIR__.'/auth.php';
