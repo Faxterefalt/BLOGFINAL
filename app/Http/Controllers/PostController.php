1e6a4d1f8bd5316c6e55d31d0605a318524269d2
Faxterefalt <azugafabian321@gmail.com>
2025-07-28 17:43:03 -0400

    listar y ver detalle de cada post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,6 +11,10 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::get();
-    return view('blog', ['posts' => $posts]);
+    return view('posts.index', ['posts' => $posts]);
+    }
+    public function show(Post $post)
+    {
+        return view('posts.show', ['post'=> $post]);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,10 @@
 <x-layout meta-title="Blog" meta-description="Blog description">
     <h1>BLOG</h1>
     @foreach($posts as $post) 
-        <h2>{{ $post->title}}</h2>
+        <h2>
+            <a href="{{route('posts.show',$post)}}">
+                {{ $post->title }}
+            </a>
+        </h2>
     @endforeach
 </x-layout>
